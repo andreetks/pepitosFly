569ead081084f49cc72d8940a15899d727d3714f
gonzalo <gonzalogarciadev@gmail.com>
2022-11-25 16:52:28 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
@@ -7,11 +7,9 @@ import Footer from "./components/Footer";
 import BackgroundTop from "./components/BackgroundTop";
 import ProductListPage from "./pages/ProductListPage";
 import ProductItemPage from "./pages/ProductItemPage";
-import CheckoutPage from "./pages/CheckoutPage"
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
-
-
   return (
     <>
       <Router>
@@ -23,7 +21,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/products" element={<ProductListPage />} />
             <Route path="/product/:id" element={<ProductItemPage />} />
-            <Route path="/checkout/:id" element={ <CheckoutPage />}  />
+            <Route path="/checkout/:id" element={<CheckoutPage />} />
           </Routes>
           <Footer />
         </div>
